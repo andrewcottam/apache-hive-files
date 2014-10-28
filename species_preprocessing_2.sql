ebe7f2131d95456e80d8662e8ae16e6a1e09eef2
Andrew Cottam <a.cottam@gmail.com>
2014-10-28 15:32:15 +0100

    added working one

--- species_preprocessing_2.sql
+++ species_preprocessing_2.sql
@@ -4,11 +4,12 @@ SET hive.input.format=org.apache.hadoop.hive.ql.io.HiveInputFormat;
 SET mapreduce.map.memory.mb=2048;
 SET mapred.map.tasks=1000;
 
-DROP TABLE species.species_2014_2_cells;
-CREATE TABLE species.species_2014_2_cells
+DROP TABLE species.species_index;
+CREATE TABLE species.species_index
 STORED AS RCFILE
 AS
 SELECT
+  objectid,
   id_no, 
   cell, 
   fully_covered, 
@@ -16,14 +17,16 @@ SELECT
   IF(cell_intersect IS NULL, geotools_cell_area(cell, 1.0, "EPSG:4326", "EPSG:54009"), geotools_area(ST_Transform("EPSG:4326", "EPSG:54009", cell_intersect))) area
 FROM (
   SELECT 
+    objectid,
     id_no, 
 	c.cell, 
 	c.fully_covered, 
 	IF(fully_covered, NULL, cell_intersects(1.0, c.cell, geom)) cell_intersect
   FROM 
-    species.species_2014_2 LATERAL VIEW geotools_cells_udtf(1.0, geom) c
+    species.species_2014_2 LATERAL VIEW esri_cells_udtf(1.0, geom) c
   ) sub;
 
 SET hive.input.format=org.apache.hadoop.hive.ql.io.CombineHiveInputFormat;
 SET mapreduce.map.java.opts=-Djava.net.preferIPv4Stack=true -Xmx825955249;
 SET mapreduce.map.memory.mb=1024;
+
